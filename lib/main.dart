f959b984016407442c118a3d4103e90a6f3a1cb4
Tania5257 <tania15-5257@diu.edu.bd>
2025-02-26 09:20:04 +0600

    fp

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Home Practice")),
+        body: Center(
+          child: Text(
+            "Welcome",
+            style: TextStyle(
+              fontSize: 50, 
+              fontWeight: FontWeight.bold, 
+              color: Colors.black, // Changed text color to blue
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
