7c626f185a7c2dd93df1bc05dd941bbd018d9157
Tania5257 <tania15-5257@diu.edu.bd>
2025-02-26 10:18:46 +0600

    hfdhd

--- lib/main.dart
+++ lib/main.dart
@@ -5,43 +5,13 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
- 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter UI',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Flutter UI"),
-        ),
-        body: Container(
-          height: double.infinity,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.star,
-                color: Colors.pink,
-                size: 60,
-              ),
-              
-              Image(
-                image: AssetImage('assets/image.jpg'),// asset name folder create korte hobe then oi folder e picture ta rakhte hobe then
-                width: 100,
-                height: 100,
-              ),
-
-              Icon(
-                Icons.star,
-                color: Colors.pink,
-                size: 60,
-              ),
-            ],
-          ),
-        ),
+        appBar: AppBar(title: Text("Flutter App")),
+        body: Center(child: Text("Welcome")),
+        
       ),
     );
   }
